6c3a4effcb1cf474a7cec33281709dd8903e9109
Juan Diego Bohorquez Quiroga <jdbq@Juans-MacBook-Air.local>
2025-11-26 13:02:42 -0500

    Vercel file updated chronium 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    outputFileTracingIncludes: {
+      'app/api/generate/route': ['./node_modules/@sparticuz/chromium/**'],
+    },
+    serverComponentsExternalPackages: ['@sparticuz/chromium'],
+  },
+  output: 'standalone', 
 };
 
 export default nextConfig;
